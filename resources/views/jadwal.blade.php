a5361425dc73df256184166754c60c69ed45e2ec
Davina T W <davinawijaya03@gmail.com>
2022-10-22 00:16:53 +0700

    jadwal hampir selesai & footer blm rapi

--- resources/views/jadwal.blade.php
+++ resources/views/jadwal.blade.php
@@ -3,76 +3,84 @@
 @section('title', 'Jadwal')
 
 @section('isi')
-<div class="m-4">
-    <h1>Jadwal Misa Harian</h1>
-    <table class="table">
-        <thead class="table-dark">
-            <tr>
-                <th scope="col">Hari</th>
-                <th scope="col">Jam</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="col">Senin - Kamis</th>
-                <th scope="col">05.00</th>
-            </tr>
-            <tr>
-                <th scope="col">Jumat</th>
-                <th scope="col">18.00</th>
-            </tr>
-        </tbody>
-    </table>
-    <h1>Jadwal Misa Mingguan</h1>
-    <table class="table">
-        <thead class="table-dark">
-            <tr>
-                <th scope="col">Hari</th>
-                <th scope="col">Jam</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="col">Sabtu</th>
-                <th scope="col">17.00</th>
-            </tr>
-            <tr>
-                <th scope="col">Minggu</th>
-                <th scope="col">06.00</th>
-            </tr>
-            <tr>
-                <th scope="col">Minggu</th>
-                <th scope="col">08.30</th>
-            </tr>
-            <tr>
-                <th scope="col">Minggu</th>
-                <th scope="col">17.00</th>
-            </tr>
-        </tbody>
-    </table>
-    <h1>Jadwal Buka Kantor Sekretariat</h1>
-    <table class="table">
-        <thead class="table-dark">
-            <tr>
-                <th scope="col">Hari</th>
-                <th scope="col">Jam</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="col">Senin</th>
-                <th scope="col">Libur</th>
-            </tr>
-            <tr>
-                <th scope="col">Selasa - Sabtu</th>
-                <th scope="col">08.00 - 15.00</th>
-            </tr>
-            <tr>
-                <th scope="col">Minggu</th>
-                <th scope="col">09.00 - 12.00</th>
-            </tr>
-        </tbody>
-    </table>
+<div class="">
+    {{-- <div> --}}
+    <h1 class="text-center">Jadwal Misa Harian</h1>
+    <div class="jadwalTable">
+        <table class="table ">
+            <thead class="bgTable">
+                <tr>
+                    <th scope="col" class="cColumn" style="padding-left: 30px; width:50%;">Hari</th>
+                    <th scope="col" class="cColumn">Jam</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Senin - Kamis</th>
+                    <th scope="col">05.00</th>
+                </tr>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Jumat</th>
+                    <th scope="col">18.00</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    {{-- </div> --}}
+    <h1 class="text-center">Jadwal Misa Mingguan</h1>
+    <div class="jadwalTable">
+        <table class="table">
+            <thead class="bgTable">
+                <tr>
+                    <th scope="col" class="cColumn" style="padding-left: 30px; width:50%;">Hari</th>
+                    <th scope="col" class="cColumn">Jam</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Sabtu</th>
+                    <th scope="col">17.00</th>
+                </tr>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Minggu</th>
+                    <th scope="col">06.00</th>
+                </tr>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Minggu</th>
+                    <th scope="col">08.30</th>
+                </tr>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Minggu</th>
+                    <th scope="col">17.00</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <h1 class="text-center">Jadwal Buka Kantor Sekretariat</h1>
+    <div class="jadwalTable">
+        <table class="table">
+            <thead class="bgTable">
+                <tr>
+                    <th scope="col" class="cColumn" style="padding-left: 30px; width:50%;">Hari</th>
+                    <th scope="col" class="cColumn">Jam</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Senin</th>
+                    <th scope="col">Libur</th>
+                </tr>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Selasa - Sabtu</th>
+                    <th scope="col">08.00 - 15.00</th>
+                </tr>
+                <tr>
+                    <th scope="col" style="padding-left: 30px;">Minggu</th>
+                    <th scope="col">09.00 - 12.00</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 
-    @endsection
+@endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,45 @@
 <section class="footer-section" id="footer">
     <div class="container">
-        <h1>Tes</h1>
+    </div>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <img src="images/logo.png" alt="" class="" height="189.18">
+                    </div>
+                    <div class="col">
+                        <h2>Gereja Katolik Santo Yusup Jember</h2>
+                    </div>
+                </div>
+                <p>( 0331 ) 486 478</p>
+                <p>styusupjember@gmail.com</p>
+                <p>Santo Yusup</p>
+            </div>
+            <div class="col order-5">
+                Rekening Kolekte :
+                BCA Jember.
+                BPGDA RK Gereja Santo Yosef
+                A/C : 024 852 1000
+
+                Rekening Sumbangan untuk Paroki :
+                Bank Mandiri Jember.
+                BPGDA Roma Katolik Gereja Santo Yosep
+                A/C : 143 00 04724 934
+            </div>
+            <div class="col order-1">
+                Rekening Pembangunan :
+                Bank Mandiri Jember
+                143 00 0559199 3
+
+
+                Rekening Pastoran untuk Intensi Misa :
+                Bank CIMB Niaga Jember
+                Herman Yoseph Suyatna, Yohanes Suparno
+                A/C : 7033 6368 5000
+            </div>
+        </div>
+    </div>
+    <div class="p-3 bg-dark text-white text-center">© 2022. Gereja Katolik Paroki Santo Yusup Jember. All Right
+        Reserved.</div>
 </section>
