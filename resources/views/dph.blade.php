a476fa009a404b1a123ce9e0c23fe647bfb4c25e
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2022-10-28 12:39:17 +0700

    checking 28

--- resources/views/dph.blade.php
+++ resources/views/dph.blade.php
@@ -3,13 +3,12 @@
 @section('title', 'DPH')
 
 @section('isi')
-    <div class="mt-4">
-        <h1 class="susunanDPH text-center" style="">Susunan Pengurus Dewan Pastoral Harian ( DPH ) Paroki Santo Yusup
-            Jember</h1>
-        <div class="d-flex justify-content-center">
-            <img src="images/dph.png" class="strukturDPH img-fluid mx-auto" width="1100">
-        </div>
-    </div>
-  
 
+    <br>
+    <h1 class="susunanDPH text-center" style="">Susunan Pengurus Dewan Pastoral Harian ( DPH ) Paroki Santo Yusup
+        Jember</h1>
+    <div class="d-flex justify-content-center">
+        <img src="images/dph.png" class="strukturDPH img-fluid mx-auto" width="1100">
+    </div>
+    <br>
 @endsection
