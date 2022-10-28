b9fac8b4a593608f9b821cbb5efc370bae2d99e7
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2022-10-28 12:26:39 +0700

    navigasi

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -43,16 +43,22 @@
 
             </ul>
 
-
-
-            <div class="justify-content-end">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+ <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon">
                     </span>
                 </button>
+
+         
+               
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                       
+
+                    </ul>
+                    
+                    <ul class="navbar-nav mb-2  mb-lg-0">
 
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="/"
